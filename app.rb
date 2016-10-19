2357092d0b1cf45df4d939f2973e569aa594ebec
natellus <natellus@yahoo.com>
2016-10-19 14:31:29 +0200

    sinatra set up + gems

---
+++ app.rb
@@ -0,0 +1,17 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:blog.db"
+
+class Post < ActiveRecord::Base
+end
+
+#class Comment < ActiveRecord::Base
+#end
+
+get '/' do
+  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+end
